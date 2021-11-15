f792dd21c5565e78258335dfc4b5752f4e40b3e1
sihun0105 <thrchtlgns@naver.com>
2021-11-15 23:08:46 +0900

    schedule list (java list 3개추가, schedule pen 수정)

---
+++ CASS_1115/app/src/main/java/com/cookandroid/a0929/List/ListViewAdapter.java
@@ -0,0 +1,90 @@
+package com.cookandroid.a0929.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.cookandroid.a0929.R;
+
+import java.util.ArrayList;
+
+public class ListViewAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private ArrayList<ListItem> listItems = new ArrayList<ListItem>();
+
+    public ListViewAdapter(Context context){
+        this.mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return listItems.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return listItems.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // item.xml 레이아웃을 inflate해서 참조획득
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        // item.xml 의 참조 획득
+        TextView txt_title = (TextView)convertView.findViewById(R.id.txt_title);
+        TextView txt_sdate = (TextView)convertView.findViewById(R.id.txt_sdate);
+        TextView txt_edate = (TextView)convertView.findViewById(R.id.txt_edate);
+        TextView txt_memo = (TextView)convertView.findViewById(R.id.txt_memo);
+        TextView txt_color = (TextView)convertView.findViewById(R.id.txt_color);
+        TextView txt_writer = (TextView)convertView.findViewById(R.id.txt_writer);
+
+        Button btn_delete = (Button)convertView.findViewById(R.id.btn_delete);
+
+        ListItem listItem = listItems.get(position);
+
+        // 가져온 데이터를 텍스트뷰에 입력
+        txt_title.setText(listItem.getTitle());
+        txt_sdate.setText(listItem.getSdate());
+        txt_edate.setText(listItem.getEdate());
+        txt_memo.setText(listItem.getMemo());
+        txt_color.setText(listItem.getColor());
+        txt_writer.setText(listItem.getWriter());
+
+        // 리스트 아이템 삭제
+        btn_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listItems.remove(position);
+                notifyDataSetChanged();
+            }
+        });
+
+        return convertView;
+    }
+
+    public void addItem(String title, String sdate, String edate, String memo, String color, String writer){
+        ListItem listItem = new ListItem();
+        listItem.setTitle(title);
+        listItem.setSdate(sdate);
+        listItem.setEdate(edate);
+        listItem.setMemo(memo);
+        listItem.setColor(color);
+        listItem.setWriter(writer);
+        listItems.add(listItem);
+    }
+}
---
+++ CASS_1115/app/src/main/java/com/cookandroid/a0929/Log_id.java
@@ -0,0 +1,29 @@
+package com.cookandroid.a0929;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Log_id extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_id);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        Button btn = (Button) findViewById(R.id.log_id_finish_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(find_pwd.this, MainActivity.class);
+//                startActivity(intent);
+                //화면에 알람 화면 띄우는 코드
+                finish();
+            }
+        });
+    };
+}
