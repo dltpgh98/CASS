534ada7871c5656be6dab396d79311e0b3dc857e
dltpgh98 <dltpgh98@gmail.com>
2021-11-25 21:22:17 +0900

    2021-11-25 스케줄 테이블 생성 및 그룹 코드 찾기 추가

---
+++ cass_app/app/src/main/java/com/cookandroid/a0929/DB/FindMemberRequest.java
@@ -0,0 +1,28 @@
+package com.cookandroid.a0929.DB;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindMemberRequest extends StringRequest {
+
+    // 서버 URL 설정 ( PHP 파일 연동 )
+    final static private String URL = "http://3.34.182.164/findmember.php";
+    private Map<String, String> map;
+
+
+    public FindMemberRequest(String user_code, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("user_code",user_code);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
